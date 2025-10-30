fc686ca70567cfc9139e6629816e250a5651c398
immaemi <nam14785@soongsil.ac.kr>
2025-10-30 23:16:31 +0900

    feat: Implement Lotto domain model
    - Define the Lotto class responsible for holding 6 unique numbers.
    - Implement comprehensive validation checks in the constructor:
        - Validate number count (must be 6).
        - Validate number type (must be integers).
        - Validate number range (1 to 45).
        - Validate for duplicate numbers.
    - Implement logic to store and return the numbers in ascending order.

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,16 +3,47 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = this.#sortLottoNumber(numbers);
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    this.#validateNumbers(numbers);
+    this.#validateDuplicateLottoNumber(numbers);
+    this.#validateRangeLottoNumber(numbers);
   }
 
-  // TODO: 추가 기능 구현
+  #validateNumbers(numbers) {
+    if (!Array.isArray(numbers) || numbers.some((n) => !Number.isInteger(n))) {
+      throw new Error("[ERROR] 로또 번호는 정수여야 합니다.");
+    }
+  }
+
+  #validateDuplicateLottoNumber(numbers) {
+    if (numbers.some((number, index, arr) => arr.indexOf(number) !== index)) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    }
+  }
+
+  #validateRangeLottoNumber(numbers) {
+    if (numbers.some(number => number < 1 || number > 45)) {
+      throw new Error("[ERROR] 로또 번호는 1 이상 45 이하의 정수여야 합니다.");
+    }
+  }
+
+  #sortLottoNumber(numbers) {
+    return [...numbers].sort((a, b) => a - b);
+  }
+
+  #getSortedLottoNumbers() {
+    return this.#sortLottoNumber(this.#numbers);
+  }
+
+  getNumbers() {
+    return this.#getSortedLottoNumbers();
+  }
 }
 
 export default Lotto;
