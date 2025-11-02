69288e620b069c1734b5702d11dd0cdec06eac3e
immaemi <nam14785@soongsil.ac.kr>
2025-11-02 13:57:32 +0900

    feat: add input trimming for lotto purchase amount
    - Remove all whitespace characters from input

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 import Lotto from "./Lotto.js";
 
 class App {
+  #trimInput(input) {
+    return input.replace(/\s/g, '');
+  }
+
   inputLottoCost = async () => {
     const lottoCost = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.\n");
-    Lotto.validateLottoCost(lottoCost);
-    return Number(lottoCost);
+    const trimmedLottoCost = this.#trimInput(lottoCost);
+    Lotto.validateLottoCost(trimmedLottoCost);
+    return Number(trimmedLottoCost);
   }
 
   async run() {
