850a9cbf88a727503299e7e923ef8ce53a26d15e
immaemi <nam14785@soongsil.ac.kr>
2025-11-02 13:45:29 +0900

    fix: store numbers without sorting

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,7 +3,7 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = this.#sortLottoNumber(numbers);
+    this.#numbers = numbers;
   }
 
   #validate(numbers) {
